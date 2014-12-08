47bbb76f3da7acea60bfea1cbf4f1883aea835b6
loic <lgevrey@oxya.com>
2014-12-08 17:21:13 +0100

    Improvement display page

--- desktop/php/camera.php
+++ desktop/php/camera.php
@@ -6,6 +6,7 @@ include_file('3rdparty', 'jquery.fileupload/jquery.ui.widget', 'js');
 include_file('3rdparty', 'jquery.fileupload/jquery.iframe-transport', 'js');
 include_file('3rdparty', 'jquery.fileupload/jquery.fileupload', 'js');
 sendVarToJS('eqType', 'camera');
+$eqLogics = eqLogic::byType('camera');
 ?>
 <div class="row row-overflow">
     <div class="col-lg-2 col-md-3 col-sm-4">
@@ -15,19 +16,47 @@ sendVarToJS('eqType', 'camera');
                 <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une caméra}}</a>
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
                 <?php
-                foreach (eqLogic::byType('camera') as $eqLogic) {
+                foreach ($eqLogics as $eqLogic) {
                     echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
                 }
                 ?>
             </ul>
         </div>
     </div>
+
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+        <legend>{{Mes caméras}}
+            <span style="font-size: 0.7em;color:#c5c5c5">
+                Vous devez être connecté à internet pour voir les prévisualisation
+            </span>
+        </legend>
+        <?php
+        if (count($eqLogics) == 0) {
+            echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>Vous n'avez aucune caméra. Cliquez sur le bouton camera à droite pour ajouter une camera</span></center>";
+        } else {
+            ?>
+            <div class="eqLogicThumbnailContainer">
+                <?php
+                foreach ($eqLogics as $eqLogic) {
+                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+                    echo "<center>";
+                    $urlPath = config::byKey('market::address') . '/market/camera/images/' . $eqLogic->getConfiguration('device') . '.jpg';
+                    echo '<img class="lazy" src="core/img/no_image.gif" data-original="' . $urlPath . '" height="105" width="95" />';
+                    echo "</center>";
+                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+                    echo '</div>';
+                }
+                ?>
+            </div>
+        <?php } ?>
+    </div>
+
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
         <div class="row">
             <div class="col-sm-6">
                 <form class="form-horizontal">
                     <fieldset>
-                        <legend>{{Général}}</legend>
+                        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}</legend>
                         <div class="form-group">
                             <label class="col-sm-3 control-label">{{Nom de l'équipement caméra}}</label>
                             <div class="col-sm-6">
@@ -72,7 +101,7 @@ sendVarToJS('eqType', 'camera');
                                 <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="port" placeholder="{{Port}}"/>
                             </div>
                         </div>
-                          <div class="form-group">
+                        <div class="form-group">
                             <label class="col-sm-3 control-label">{{IP Externe}}</label>
                             <div class="col-sm-4">
                                 <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ip_ext" placeholder="{{IP}}"/>
