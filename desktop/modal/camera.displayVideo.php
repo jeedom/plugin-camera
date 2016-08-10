15ea181482b137f84079a9aa542a30a2ca4a55d2
Ludovic <ludovic@jeedom.com>
2016-08-10 02:36:45 +0200

    added loop

--- desktop/modal/camera.displayVideo.php
+++ desktop/modal/camera.displayVideo.php
@@ -2,7 +2,7 @@
 if (!isConnect()) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
-echo '<video width="1180" height="664" controls autoplay>
+echo '<video width="1180" height="664" controls autoplay loop>
   <source src="' . init('src') . '">
 Your browser does not support the video tag.
 </video>';
