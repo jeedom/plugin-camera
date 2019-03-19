aabd23688304771de33c9057932d8d98015392c6
Loïc <loic@jeedom.com>
2019-03-19 12:13:55 +0100

    change fa to fas

--- desktop/php/camera.php
+++ desktop/php/camera.php
@@ -302,7 +302,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 	
 	
 	<div role="tabpanel" class="tab-pane" id="commandtab">
-		<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Ajouter une commande}}</a><br/><br/>
+		<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fas fa-plus-circle"></i> {{Ajouter une commande}}</a><br/><br/>
 		<table id="table_cmd" class="table table-bordered table-condensed">
 			<thead>
 				<tr>
