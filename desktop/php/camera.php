4389196d6e6dc4d138a6f1c9c80cc5648cfda65c
Loïc <loic@jeedom.com>
2019-03-19 13:42:51 +0100

    chnage fa to fas

--- desktop/php/camera.php
+++ desktop/php/camera.php
@@ -8,7 +8,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 ?>
 <div class="row row-overflow">
 	<div class="col-xs-12 eqLogicThumbnailDisplay">
-		<legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+		<legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
 		<div class="eqLogicThumbnailContainer">
 			<div class="cursor eqLogicAction logoPrimary" data-action="add" >
 				<center>
@@ -52,16 +52,16 @@ $eqLogics = eqLogic::byType($plugin->getId());
 	<div class="col-xs-12 eqLogic" style="display: none;">
 		<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
-				<a class="btn btn-default btn-sm eqLogicAction roundedLeft" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fa fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+				<a class="btn btn-default btn-sm eqLogicAction roundedLeft" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
 			</span>
 		</div>
 		<ul class="nav nav-tabs" role="tablist">
-			<li role="presentation"><a class="eqLogicAction cursor" aria-controls="home" role="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
-			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
-			<li role="presentation"><a href="#displaytab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Image}}</a></li>
+			<li role="presentation"><a class="eqLogicAction cursor" aria-controls="home" role="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
+			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
+			<li role="presentation"><a href="#displaytab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Image}}</a></li>
 			<li role="presentation"><a href="#capturetab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-video-camera"></i> {{Capture}}</a></li>
 			<li role="presentation"><a href="#alimtab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-power-off"></i> {{Alimentation}}</a></li>
-			<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+			<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
 		</ul>
 		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
@@ -282,7 +282,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						<div class="input-group">
 							<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commandOn"/>
 							<span class="input-group-btn">
-								<a class="btn btn-default listCmdActionOther"><i class="fa fa-list-alt"></i></a>
+								<a class="btn btn-default listCmdActionOther"><i class="fas fa-list-alt"></i></a>
 							</span>
 						</div>
 					</div>
@@ -291,7 +291,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						<div class="input-group">
 							<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commandOff"/>
 							<span class="input-group-btn">
-								<a class="btn btn-default listCmdActionOther"><i class="fa fa-list-alt"></i></a>
+								<a class="btn btn-default listCmdActionOther"><i class="fas fa-list-alt"></i></a>
 							</span>
 						</div>
 					</div>
