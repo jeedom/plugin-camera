5647a4a128789c67c1337204263085bca8b8dd9c
Loïc <loic@jeedom.com>
2019-05-09 14:19:11 +0200

    fix icon

--- desktop/modal/camera.history.php
+++ desktop/modal/camera.history.php
@@ -33,8 +33,8 @@ krsort($files);
 ?>
 <div id='div_cameraRecordAlert' style="display: none;"></div>
 <?php
-echo '<a class="btn btn-danger bt_removeCameraFile pull-right" data-all="1" data-filename="' . $camera->getId() . '/*"><i class="fa fa-trash-o"></i> {{Tout supprimer}}</a>';
-echo '<a class="btn btn-success  pull-right" target="_blank" href="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/*') . '" ><i class="fa fa-download"></i> {{Tout télécharger}}</a>';
+echo '<a class="btn btn-danger bt_removeCameraFile pull-right" data-all="1" data-filename="' . $camera->getId() . '/*"><i class="fas fa-trash"></i> {{Tout supprimer}}</a>';
+echo '<a class="btn btn-success  pull-right" target="_blank" href="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/*') . '" ><i class="fas fa-download"></i> {{Tout télécharger}}</a>';
 ?>
 <?php
 $i = 0;
@@ -42,28 +42,28 @@ foreach ($files as $date => &$file) {
 	$cameraName = str_replace(' ', '-', $camera->getName());
 	echo '<div class="div_dayContainer">';
 	echo '<legend>';
-	echo '<a class="btn btn-xs btn-danger bt_removeCameraFile" data-day="1" data-filename="' . $camera->getId() . '/' . $cameraName . '_' . $date . '*"><i class="fa fa-trash-o"></i> {{Supprimer}}</a> ';
-	echo '<a class="btn btn-xs btn-success" target="_blank"  href="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/' . $cameraName . '_' . $date . '*') . '" ><i class="fa fa-download"></i> {{Télécharger}}</a> ';
+	echo '<a class="btn btn-xs btn-danger bt_removeCameraFile" data-day="1" data-filename="' . $camera->getId() . '/' . $cameraName . '_' . $date . '*"><i class="fas fa-trash"></i> {{Supprimer}}</a> ';
+	echo '<a class="btn btn-xs btn-success" target="_blank"  href="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/' . $cameraName . '_' . $date . '*') . '" ><i class="fas fa-download"></i> {{Télécharger}}</a> ';
 	echo '<span class="cameraHistoryDate">'.$date.'</span>';
 	echo ' <a class="btn btn-xs btn-default toggleList"><i class="fa fa-chevron-down"></i></a> ';
 	echo '</legend>';
 	echo '<div class="cameraThumbnailContainer">';
 	krsort($file);
 	foreach ($file as $time => $filename) {
-		$fontType = 'fa-camera';
+		$fontType = 'fas-camera';
 		if (strpos($filename, '.mp4')) {
-			$fontType = 'fa-video-camera';
+			$fontType = 'fas-video-camera';
 			$i++;
 		}
-		echo '<div class="cameraDisplayCard" style="background-color: #e7e7e7;padding:5px;height:167px;">';
+		echo '<div class="cameraDisplayCard" style="padding:5px;height:170px;">';
 		echo '<center><i class="fa ' . $fontType . ' pull-right"></i>  ' . str_replace('-', ':', $time) . '</center>';
 		if (strpos($filename, '.mp4')) {
 			echo '<video class="displayVideo" width="150" height="100" controls loop data-src="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/' . $filename) . '" style="cursor:pointer"><source src="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/' . $filename) . '">Your browser does not support the video tag.</video>';
 		} else {
-			echo '<center><img class="img-responsive cursor displayImage lazy" src="plugins/camera/core/img/no-image.png" data-original="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/' . $filename) . '" width="150"/></center>';
+			echo '<center><img class="img-responsive cursor displayImage lazy" src="plugins/camera/core/img/no-image.png" data-original="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/' . $filename) . '" width="150" style="max-height:80px;"/></center>';
 		}
-		echo '<center style="margin-top:5px;"><a target="_blank" href="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/' . $filename) . '" class="btn btn-success btn-xs" style="color : white"><i class="fa fa-download"></i></a>';
-		echo ' <a class="btn btn-danger bt_removeCameraFile btn-xs" style="color : white" data-filename="' . $camera->getId() . '/' . $filename . '"><i class="fa fa-trash-o"></i></a></center>';
+		echo '<center style="margin-top:5px;"><a target="_blank" href="core/php/downloadFile.php?pathfile=' . urlencode($dir . '/' . $filename) . '" class="btn btn-success btn-xs" style="color : white"><i class="fas fa-download"></i></a>';
+		echo ' <a class="btn btn-danger bt_removeCameraFile btn-xs" style="color : white" data-filename="' . $camera->getId() . '/' . $filename . '"><i class="fas fa-trash"></i></a></center>';
 		echo '</div>';
 	}
 	echo '</div>';
@@ -71,55 +71,55 @@ foreach ($files as $date => &$file) {
 }
 ?>
 <script>
-	$('.cameraThumbnailContainer').packery({gutter : 5});
-	$('.displayImage').on('click', function() {
-		$('#md_modal2').dialog({title: "Image"});
-		$('#md_modal2').load('index.php?v=d&plugin=camera&modal=camera.displayImage&src='+ $(this).attr('src')).dialog('open');
-	});
-	$('.displayVideo').on('click', function() {
-		$('#md_modal2').dialog({title: "Vidéo"});
-		$('#md_modal2').load('index.php?v=d&plugin=camera&modal=camera.displayVideo&src='+ $(this).attr('data-src')).dialog('open');
-	});
-	$('.bt_removeCameraFile').on('click', function() {
-		var filename = $(this).attr('data-filename');
-		var card = $(this).closest('.cameraDisplayCard');
-		if($(this).attr('data-day') == 1){
-			card = $(this).closest('.div_dayContainer');
-		}
-		if($(this).attr('data-all') == 1){
-			card = $('.div_dayContainer');
-		}
-		$.ajax({
-			type: "POST",
-			url: "plugins/camera/core/ajax/camera.ajax.php",
-			data: {
-				action: "removeRecord",
-				file: filename,
-			},
-			dataType: 'json',
-			error: function(request, status, error) {
-				handleAjaxError(request, status, error,$('#div_cameraRecordAlert'));
-			},
-			success: function(data) {
-				if (data.state != 'ok') {
-					$('#div_cameraRecordAlert').showAlert({message: data.result, level: 'danger'});
-					return;
-				}
-				card.remove();
-				$(".cameraThumbnailContainer").slideToggle(1);
-				$('.cameraThumbnailContainer').packery({gutter : 5});
-				$(".cameraThumbnailContainer").slideToggle(1);
+$('.cameraThumbnailContainer').packery({gutter : 5});
+$('.displayImage').on('click', function() {
+	$('#md_modal2').dialog({title: "Image"});
+	$('#md_modal2').load('index.php?v=d&plugin=camera&modal=camera.displayImage&src='+ $(this).attr('src')).dialog('open');
+});
+$('.displayVideo').on('click', function() {
+	$('#md_modal2').dialog({title: "Vidéo"});
+	$('#md_modal2').load('index.php?v=d&plugin=camera&modal=camera.displayVideo&src='+ $(this).attr('data-src')).dialog('open');
+});
+$('.bt_removeCameraFile').on('click', function() {
+	var filename = $(this).attr('data-filename');
+	var card = $(this).closest('.cameraDisplayCard');
+	if($(this).attr('data-day') == 1){
+		card = $(this).closest('.div_dayContainer');
+	}
+	if($(this).attr('data-all') == 1){
+		card = $('.div_dayContainer');
+	}
+	$.ajax({
+		type: "POST",
+		url: "plugins/camera/core/ajax/camera.ajax.php",
+		data: {
+			action: "removeRecord",
+			file: filename,
+		},
+		dataType: 'json',
+		error: function(request, status, error) {
+			handleAjaxError(request, status, error,$('#div_cameraRecordAlert'));
+		},
+		success: function(data) {
+			if (data.state != 'ok') {
+				$('#div_cameraRecordAlert').showAlert({message: data.result, level: 'danger'});
+				return;
 			}
-		});
+			card.remove();
+			$(".cameraThumbnailContainer").slideToggle(1);
+			$('.cameraThumbnailContainer').packery({gutter : 5});
+			$(".cameraThumbnailContainer").slideToggle(1);
+		}
 	});
+});
 
-	$(".cameraThumbnailContainer").slideToggle(1);
-	$(".cameraThumbnailContainer").eq(0).slideToggle(1);
-	$('.toggleList').on('click', function() {
-		$(this).closest('.div_dayContainer').find(".cameraThumbnailContainer").slideToggle("slow");
-	});
+$(".cameraThumbnailContainer").slideToggle(1);
+$(".cameraThumbnailContainer").eq(0).slideToggle(1);
+$('.toggleList').on('click', function() {
+	$(this).closest('.div_dayContainer').find(".cameraThumbnailContainer").slideToggle("slow");
+});
 
-	$("img.lazy").lazyload({
-		container: $("#md_modal")
-	});
+$("img.lazy").lazyload({
+	container: $("#md_modal")
+});
 </script>
