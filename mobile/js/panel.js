71f09acebb7cc336a537522d12e453422b968c08
Loïc <loic@jeedom.com>
2019-09-22 09:51:34 +0200

    improvements

--- mobile/js/panel.js
+++ mobile/js/panel.js
@@ -71,7 +71,7 @@ function displayCamera(_object_id) {
         $('#div_displayEquipementCamera').append(data.result.eqLogics[i]).trigger('create');
       }
       setTileSize('.eqLogic');
-      jeedom.eqLogic.changeDisplayObjectName(true);
+      $('.eqLogic-widget').addClass('displayObjectName');
       $('#div_displayEquipementCamera').packery({gutter : 0});
       $('#div_displayEquipementCamera').packery({gutter : 0});
       $.hideLoading();
