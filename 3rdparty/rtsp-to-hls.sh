93cae1aba2ce56c8c11d2c928e818ac69dc687bd
tmartinez <tmartinez69009@gmail.com>
2020-05-24 00:44:53 +0200

    tuning

--- 3rdparty/rtsp-to-hls.sh
+++ 3rdparty/rtsp-to-hls.sh
@@ -13,8 +13,10 @@ HLS_LIST_SIZE=${HLS_LIST_SIZE:-5}
 ffmpeg -i "$SOURCE" \
         -f hls \
         -vsync 0 -copyts -vcodec copy -acodec copy \
+        -tune zerolatency \
         -movflags frag_keyframe+empty_moov \
         -hls_flags delete_segments+append_list \
+        -hls_init_time 1 \
         -hls_time $HLS_TIME \
         -hls_list_size $HLS_LIST_SIZE \
         -hls_base_url "../data/segments/" \
