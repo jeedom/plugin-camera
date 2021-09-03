9a331566d4a891b2c27f0f565570e1d6bf7b42d1
loic <loic@jeedom.com>
2021-09-03 15:37:19 +0200

    reinforce security

--- desktop/modal/camera.displayImage.php
+++ desktop/modal/camera.displayImage.php
@@ -2,5 +2,10 @@
 if (!isConnect()) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
+if (!substr_compare(init('src'), '.jpg', -strlen('.jpg'))) {
+	throw new Exception('{{401 - Accès non autorisé}}');
+}
+if (strpos(init('src'), 'camera') === false) {
+	throw new Exception('{{401 - Accès non autorisé}}');
+}
 echo '<center><img class="img-responsive" src="' . init('src') . '" /></center>';
-?>
--- desktop/modal/camera.displayVideo.php
+++ desktop/modal/camera.displayVideo.php
@@ -2,8 +2,13 @@
 if (!isConnect()) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
+if (!substr_compare(init('src'), '.mp4', -strlen('.mp4'))) {
+	throw new Exception('{{401 - Accès non autorisé}}');
+}
+if (strpos(init('src'), 'camera') === false) {
+	throw new Exception('{{401 - Accès non autorisé}}');
+}
 echo '<video width="1180" height="664" controls autoplay loop>
   <source src="' . init('src') . '">
 Your browser does not support the video tag.
 </video>';
-?>
